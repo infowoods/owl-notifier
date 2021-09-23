440d94670dafa46152f027323c0518b6d6e7b476
xingchi <bianxingchi@wecash.net>
2021-09-23 21:58:11 +0800

    remove target && add export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require('path')
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-  target: "experimental-serverless-trace",
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
