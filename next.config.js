13205b8a7577179d7f0b440ca176c45ae72f3533
xingchi <bianxingchi@wecash.net>
2021-09-23 21:26:33 +0800

    add target

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const path = require('path')
-// const { i18n } = require('./next-i18next.config')
+const { i18n } = require('./next-i18next.config')
 
 module.exports = {
+  target: "experimental-serverless-trace",
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -10,8 +11,5 @@ module.exports = {
   images: {
     domains: ['mixin-images.zeromesh.net'],
   },
-  i18n: {
-    defaultLocale: 'zh',
-    locales: ['zh', 'en'],
-  },
+  i18n,
 }
