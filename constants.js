9cbb720476e6579ab574db6b29c395a6ec9d647d
xingchi <bianxingchi@wecash.net>
2021-10-01 13:24:35 +0800

    update client_id

--- constants.js
+++ constants.js
@@ -2,7 +2,8 @@ export const isProduct = process.env.APP_ENV === 'prod'
 
 // export const MIXIN_CLIENT_ID = process.env.MIXIN_CLIENT_ID
 // export const MIXIN_CLIENT_ID = 'b9fee5b9-b3cb-4448-9f31-a6440ba21bf8' // 没时间
-export const MIXIN_CLIENT_ID = '8a004350-fefe-45ac-8411-5e62be2e377e' // owl test bot
+// export const MIXIN_CLIENT_ID = '8a004350-fefe-45ac-8411-5e62be2e377e' // owl test bot
+export const MIXIN_CLIENT_ID = '60d30faa-3825-48da-ac1c-6aca1f573dd5' // owl
 
 export const MIXIN_API_HOST = 'https://mixin-api.zeromesh.net'
 
