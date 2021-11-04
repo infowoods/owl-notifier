625500b598bfce9219a96af412e2197495aac612
xingchi <bianxingchi@wecash.net>
2021-11-04 19:32:55 +0800

    fix loading

--- components/Home/index.js
+++ components/Home/index.js
@@ -114,6 +114,7 @@ function Home() {
     } catch (error) {
       if (error?.action === 'logout') {
         toast.error(t('auth_expire'))
+        setLoading(false)
         logout(dispatch)
         return
       }
